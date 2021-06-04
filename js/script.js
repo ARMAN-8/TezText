2bd81be3cc450834252bec4ec935f95e1f0dcc2e
Your Name <you@abc.net>
2021-06-04 17:03:02 +0600

    final chages

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@
 
 'use strict';
 
+// grab everything we need
+
+const btn = document.querySelector('button.mobile-menu-button');
+const menu = document.querySelector('.mobile-menu');
+
+btn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+});
